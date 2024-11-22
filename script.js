431173b9d906e76cb54cb13105090f5e889547e3
Lanceylot2 <lancesantiago691@gmail.com>
2024-11-22 04:35:30 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function changebackground() {
+    document.body.style.backgroundColor = "blue";
+    document.body.style.fontSize = "20px"; 
+    document.getElementById("myImage").src = "./images/boy.jpg"; 
+}
+function pink() {
+    document.body.style.backgroundColor = "pink";
+    document.body.style.fontSize = "20px"; 
+    document.getElementById("myImage").src = "./images/girl.jpg"; 
+}
+function brown() {
+    document.body.style.backgroundColor = "brown";
+    document.body.style.fontSize = "50px";
+    document.getElementById("myImage").src = "./images/old.jpg";
+}
